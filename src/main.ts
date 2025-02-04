e662734cdbc3462c7b62de84ddaa6d233fba0852
vlad424 <vlad6754687@gmail.com>
2025-02-04 17:03:40 +0700

    main | connected prisma

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 5000
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors()
+
+  await app.listen(5000, () => console.log(`server starts on ${PORT} port`))
 }
 bootstrap();
